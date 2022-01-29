05df7d14c2dc6d91fa3d9117f83be61dc3da4895
miroshnykov <dmytro.miroshnykov@actionmediamtl.com>
2022-01-28 20:21:05 -0500

    free memory

--- freeMemoryUbuntu.sh
+++ freeMemoryUbuntu.sh
@@ -3,6 +3,7 @@
 free -h && sudo sysctl -w vm.drop_caches=3 && sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches && free -h
 
 available=$(free -mw | awk '/^Mem:/{print $8}')
-title="Low memory! $available MB available"
+timestamp=$(date +%s)
+title="Clean up memory! available:$available MB time $timestamp"
 echo "$title"
 #notify-send "$title" 'память очщена'
